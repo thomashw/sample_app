c478b81a3200f7d840148abf7f999cc8854c07fc
Thomas Hewton-Waters <thomashw@me.com>
2012-11-28 20:11:37 -0700

    Add user signup functionality
    Signed-off-by: Thomas Hewton-Waters <thomashw@me.com>

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,4 +18,28 @@ describe "UserPages" do
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text:user.name) }
   end
+
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Thomas"
+        fill_in "Email", with: "thomashw@me.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Password confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
