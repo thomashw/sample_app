4cc83a1b60f68bfdef148686aae3a5620ff8f025
Thomas Hewton-Waters <thomashw@me.com>
2012-11-29 20:18:17 -0700

    Replace "Password digest" with "Password" in error message
    Presence validation for password field has been removed because has_secure_password has a presence validation and having both resulted in two of the same error message when the password wasn't filled in.
    
    Signed-off-by: Thomas Hewton-Waters <thomashw@me.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
